d3a4f2517ff835596cd8ebbeb06f3229caa1fbfe
vijayavalluri <vvvijaya56@gmail.com>
2023-07-26 13:57:34 +0200

    'stephan changes included with files'

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,42 +1,49 @@
+const questions = [
+  {
+    text: "What is the highest peak in Germany?",
+    options: [
+      { id: 0, text: "Medium Weather Peak", isCorrect: false },
+      { id: 1, text: "Schneefernerkopf", isCorrect: false },
+      { id: 2, text: "Hochwanner", isCorrect: false },
+      { id: 3, text: "Zugspitze", isCorrect: true },
+    ],
+  },
+  {
+    text: "Palm Island is in which Country?",
+    options: [
+      { id: 0, text: "United Arab Emirates - Dubai", isCorrect: true },
+      { id: 1, text: "United states of America - Florida", isCorrect: false },
+      { id: 2, text: "South America", isCorrect: false },
+      { id: 3, text: "North America", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the largest state in the germany?",
+    options: [
+      { id: 0, text: "Bavaria", isCorrect: true },
+      { id: 1, text: "Niedersachsen", isCorrect: false },
+      { id: 2, text: "Baden-Württemberg", isCorrect: false },
+      { id: 3, text: "Hessen", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the largest state in the US?",
+    options: [
+      { id: 0, text: "California", isCorrect: false },
+      { id: 1, text: "Alaska", isCorrect: true },
+      { id: 2, text: "Texas", isCorrect: false },
+      { id: 3, text: "Montana", isCorrect: false },
+    ],
+  },
+  {
+    text: "Which of the following countries DO NOT border the US?",
+    options: [
+      { id: 0, text: "Canada", isCorrect: false },
+      { id: 1, text: "Russia", isCorrect: true },
+      { id: 2, text: "Cuba", isCorrect: true },
+      { id: 3, text: "Mexico", isCorrect: false },
+    ],
+  },
+];
 
-function Questions() {
-
-
-return (
-    <div>
-    <h2>
-    <h3 className="question-text">What is the highest peak in Germany?</h3>
-    <ul>
-      <li>Schneefernerkopf</li>
-      <li>Medium Weather Peak</li>
-      <li>Zugspitze</li>
-      <li>Hochwanner</li>
-    </ul>
-    </h2>
-
-
-    <h2>
-    <h3 className="question-text">Palm Island is in which Country?</h3>
-    <ul>
-      <li>United states of America - Florida</li>
-      <li>South America</li>
-      <li>North America</li>
-      <li>United Arab Emirates - Dubai</li>
-    </ul>
-    </h2>
-
-    <h2>
-    <h3 className="question-text">What is the largest state in the germany?</h3>
-    <ul>
-      <li>Bavaria</li>
-      <li> Niedersachsen</li>
-      <li> Baden-Württemberg</li>
-      <li> Hessen</li>
-    </ul>
-    </h2>
-
-    
-  </div>
-);
-}
-export default Questions
+export default questions;
