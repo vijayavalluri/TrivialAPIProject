036e95ba005c1656f370ccef4cd0f516571cb95e
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-26 14:55:40 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import questions from './components/Questions'
+import axios from 'axios'
+// import questions from './components/Questions'
 
 // function App() {
 //   // Properties
@@ -88,12 +89,79 @@ function App() {
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState([
+    {
+      id: '26c4806d-fe05-4344-84ca-3c0092d15477',
+      question: 'What does CPU stand for?',
+      correctAnswer: 'Central Processing Unit',
+      answers: [
+        'Central Processing Unit',
+        'Computer Personal Unit',
+        'Central Process Unit',
+        'Central Processor Unit',
+      ],
+      userAnswer: '',
+    },
+    {
+      id: 'cd647d49-62e0-4ff9-8b4d-71e0b4099767',
+      question: 'The following Spanish provinces are located in the northern area of Spain except:',
+      correctAnswer: 'Murcia',
+      answers: ['Le&oacute;n', 'Asturias', 'Navarre', 'Murcia'],
+      userAnswer: '',
+    },
+    {
+      id: 'd52c4a1c-8c33-4308-831b-916fa989ef21',
+      question: 'What is the name of the first &quot;Star Wars&quot; film by release order?',
+      correctAnswer: 'A New Hope',
+      answers: ['The Force Awakens', 'The Phantom Menace', 'A New Hope', 'Revenge of the Sith'],
+      userAnswer: '',
+    },
+    {
+      id: '258d5be5-7e3b-44cc-bbb1-fb6c1a03bbda',
+      question:
+        'The metric prefix &quot;atto-&quot; makes a measurement how much smaller than the base unit?',
+      correctAnswer: 'One Quintillionth',
+      answers: ['One Septillionth', 'One Quintillionth', 'One Billionth', 'One Quadrillionth'],
+      userAnswer: '',
+    },
+    {
+      id: '179e8915-189d-4643-8a52-5801a5a1fc84',
+      question: 'Pamina and Papageno are characters in what Mozart opera?',
+      correctAnswer: 'The Magic Flute',
+      answers: ['The Magic Flute', 'The Marriage of Figaro', 'The Goose of Cairo', 'The Impresario'],
+      userAnswer: '',
+    },
+    {
+      id: '4e7f9530-93d4-4d73-9ee5-75d9267b2a7d',
+      question: 'Which of the following awards do Matt Stone and Trey Parker NOT have?',
+      correctAnswer: 'Oscar',
+      answers: ['Tony', 'Oscar', 'Grammy', 'Emmy'],
+      userAnswer: '',
+    },])
 
+  useEffect(()=>{
+    getQuestions()
+  }, [])
+  
+  const getQuestions = () => {
+    axios.get('https://wd40-trivia.onrender.com/api/questions')
+    .then(res=> {
+      console.log(res.data)
+      setQuestions(res.data)
+    })
+    .catch(error=> console.log(error))
+  }
 
   // Helper Functions
 
   /* A possible answer was clicked */
   const optionClicked = (obj, text) => {
+    // get all the ones in the arra
+    let payload = questions.filter(element => element.id != obj.id)
+    payload = [...payload, {...obj, userAnswer: text}]
+
+    // change the questions array userAnswet in the corresponding object
+    setQuestions(payload)
 
     // Increment the score
     if (obj.correctAnswer == text) {
@@ -112,9 +180,15 @@ function App() {
     setScore(0);
     setCurrentQuestion(0);
     setShowResults(false);
+    getQuestions()
   };
 
   return (
+    <>
+    {
+      !questions ?
+    <p>Loading</p>
+    :
     <div className="App">
       {/* 1. Header  */}
       <h1>Quiz</h1>
@@ -157,7 +231,8 @@ function App() {
           </ul>
         </div>
       )}
-    </div>
+    </div>}
+    </>
   );
 }
 
