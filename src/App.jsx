8a2062633b7365f91354a610393a6b3d8478303e
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-26 14:32:06 +0200

    Fixed the  questions file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,87 @@ import React, { useState } from "react";
 import "./App.css";
 import questions from './components/Questions'
 
+// function App() {
+//   // Properties
+//   const [showResults, setShowResults] = useState(false);
+//   const [currentQuestion, setCurrentQuestion] = useState(0);
+//   const [score, setScore] = useState(0);
+
+
+//   // Helper Functions
+
+//   /* A possible answer was clicked */
+//   const optionClicked = (isCorrect) => {
+//     // Increment the score
+//     if (isCorrect) {
+//       setScore(score + 1);
+//     }
+
+//     if (currentQuestion + 1 < questions.length) {
+//       setCurrentQuestion(currentQuestion + 1);
+//     } else {
+//       setShowResults(true);
+//     }
+//   };
+
+//   /* Resets the game back to default */
+//   const restartGame = () => {
+//     setScore(0);
+//     setCurrentQuestion(0);
+//     setShowResults(false);
+//   };
+
+//   return (
+//     <div className="App">
+//       {/* 1. Header  */}
+//       <h1>Quiz</h1>
+
+//       {/* 2. Current Score  */}
+//       <h2>Score: {score}</h2>
+
+//       {/* 3. Show results or show the question game  */}
+//       {showResults ? (
+//         /* 4. Final Results */
+//         <div className="final-results">
+//           <h1>Final Results</h1>
+//           <h2>
+//             {score} out of {questions.length} correct - (
+//             {(score / questions.length) * 100}%)
+//           </h2>
+//           <button onClick={() => restartGame()}>Restart game</button>
+//         </div>
+//       ) : (
+//         /* 5. Question Card  */
+//         <div className="question-card">
+//           {/* Current Question  */}
+//           <h2>
+//             Question: {currentQuestion + 1} out of {questions.length}
+//           </h2>
+//           <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+//           {/* List of possible answers  */}
+//           <ul>
+//             {questions[currentQuestion].options.map((option) => {
+//               return (
+//                 <li
+//                   key={option.id}
+//                   onClick={() => optionClicked(option.isCorrect)}
+//                 >
+//                   {option.text}
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// ECA'S VERSION
 function App() {
   // Properties
   const [showResults, setShowResults] = useState(false);
@@ -12,9 +93,10 @@ function App() {
   // Helper Functions
 
   /* A possible answer was clicked */
-  const optionClicked = (isCorrect) => {
+  const optionClicked = (obj, text) => {
+
     // Increment the score
-    if (isCorrect) {
+    if (obj.correctAnswer == text) {
       setScore(score + 1);
     }
 
@@ -58,17 +140,17 @@ function App() {
           <h2>
             Question: {currentQuestion + 1} out of {questions.length}
           </h2>
-          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+          <h3 className="question-text">{questions[currentQuestion].question}</h3>
 
           {/* List of possible answers  */}
           <ul>
-            {questions[currentQuestion].options.map((option) => {
+            {questions[currentQuestion].answers.map((option) => {
               return (
                 <li
-                  key={option.id}
-                  onClick={() => optionClicked(option.isCorrect)}
+                  // key={option.id}
+                  onClick={() => optionClicked(questions[currentQuestion], option)}
                 >
-                  {option.text}
+                  {option}
                 </li>
               );
             })}
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,48 +1,101 @@
+// const questions = [
+//   {
+//     text: "What is the highest peak in Germany?",
+//     options: [
+//       { id: 0, text: "Medium Weather Peak", isCorrect: false },
+//       { id: 1, text: "Schneefernerkopf", isCorrect: false },
+//       { id: 2, text: "Hochwanner", isCorrect: false },
+//       { id: 3, text: "Zugspitze", isCorrect: true },
+//     ],
+//   },
+//   {
+//     text: "Palm Island is in which Country?",
+//     options: [
+//       { id: 0, text: "United Arab Emirates - Dubai", isCorrect: true },
+//       { id: 1, text: "United states of America - Florida", isCorrect: false },
+//       { id: 2, text: "South America", isCorrect: false },
+//       { id: 3, text: "North America", isCorrect: false },
+//     ],
+//   },
+//   {
+//     text: "What is the largest state in the germany?",
+//     options: [
+//       { id: 0, text: "Bavaria", isCorrect: true },
+//       { id: 1, text: "Niedersachsen", isCorrect: false },
+//       { id: 2, text: "Baden-Württemberg", isCorrect: false },
+//       { id: 3, text: "Hessen", isCorrect: false },
+//     ],
+//   },
+//   {
+//     text: "What is the largest state in the US?",
+//     options: [
+//       { id: 0, text: "California", isCorrect: false },
+//       { id: 1, text: "Alaska", isCorrect: true },
+//       { id: 2, text: "Texas", isCorrect: false },
+//       { id: 3, text: "Montana", isCorrect: false },
+//     ],
+//   },
+//   {
+//     text: "Which of the following countries DO NOT border the US?",
+//     options: [
+//       { id: 0, text: "Canada", isCorrect: false },
+//       { id: 1, text: "Russia", isCorrect: true },
+//       { id: 2, text: "Cuba", isCorrect: true },
+//       { id: 3, text: "Mexico", isCorrect: false },
+//     ],
+//   },
+// ];
+
+// export default questions;
+
 const questions = [
   {
-    text: "What is the highest peak in Germany?",
-    options: [
-      { id: 0, text: "Medium Weather Peak", isCorrect: false },
-      { id: 1, text: "Schneefernerkopf", isCorrect: false },
-      { id: 2, text: "Hochwanner", isCorrect: false },
-      { id: 3, text: "Zugspitze", isCorrect: true },
+    id: '26c4806d-fe05-4344-84ca-3c0092d15477',
+    question: 'What does CPU stand for?',
+    correctAnswer: 'Central Processing Unit',
+    answers: [
+      'Central Processing Unit',
+      'Computer Personal Unit',
+      'Central Process Unit',
+      'Central Processor Unit',
     ],
+    userAnswer: '',
   },
   {
-    text: "Palm Island is in which Country?",
-    options: [
-      { id: 0, text: "United Arab Emirates - Dubai", isCorrect: true },
-      { id: 1, text: "United states of America - Florida", isCorrect: false },
-      { id: 2, text: "South America", isCorrect: false },
-      { id: 3, text: "North America", isCorrect: false },
-    ],
+    id: 'cd647d49-62e0-4ff9-8b4d-71e0b4099767',
+    question: 'The following Spanish provinces are located in the northern area of Spain except:',
+    correctAnswer: 'Murcia',
+    answers: ['Le&oacute;n', 'Asturias', 'Navarre', 'Murcia'],
+    userAnswer: '',
   },
   {
-    text: "What is the largest state in the germany?",
-    options: [
-      { id: 0, text: "Bavaria", isCorrect: true },
-      { id: 1, text: "Niedersachsen", isCorrect: false },
-      { id: 2, text: "Baden-Württemberg", isCorrect: false },
-      { id: 3, text: "Hessen", isCorrect: false },
-    ],
+    id: 'd52c4a1c-8c33-4308-831b-916fa989ef21',
+    question: 'What is the name of the first &quot;Star Wars&quot; film by release order?',
+    correctAnswer: 'A New Hope',
+    answers: ['The Force Awakens', 'The Phantom Menace', 'A New Hope', 'Revenge of the Sith'],
+    userAnswer: '',
   },
   {
-    text: "What is the largest state in the US?",
-    options: [
-      { id: 0, text: "California", isCorrect: false },
-      { id: 1, text: "Alaska", isCorrect: true },
-      { id: 2, text: "Texas", isCorrect: false },
-      { id: 3, text: "Montana", isCorrect: false },
-    ],
+    id: '258d5be5-7e3b-44cc-bbb1-fb6c1a03bbda',
+    question:
+      'The metric prefix &quot;atto-&quot; makes a measurement how much smaller than the base unit?',
+    correctAnswer: 'One Quintillionth',
+    answers: ['One Septillionth', 'One Quintillionth', 'One Billionth', 'One Quadrillionth'],
+    userAnswer: '',
   },
   {
-    text: "Which of the following countries DO NOT border the US?",
-    options: [
-      { id: 0, text: "Canada", isCorrect: false },
-      { id: 1, text: "Russia", isCorrect: true },
-      { id: 2, text: "Cuba", isCorrect: true },
-      { id: 3, text: "Mexico", isCorrect: false },
-    ],
+    id: '179e8915-189d-4643-8a52-5801a5a1fc84',
+    question: 'Pamina and Papageno are characters in what Mozart opera?',
+    correctAnswer: 'The Magic Flute',
+    answers: ['The Magic Flute', 'The Marriage of Figaro', 'The Goose of Cairo', 'The Impresario'],
+    userAnswer: '',
+  },
+  {
+    id: '4e7f9530-93d4-4d73-9ee5-75d9267b2a7d',
+    question: 'Which of the following awards do Matt Stone and Trey Parker NOT have?',
+    correctAnswer: 'Oscar',
+    answers: ['Tony', 'Oscar', 'Grammy', 'Emmy'],
+    userAnswer: '',
   },
 ];
 
