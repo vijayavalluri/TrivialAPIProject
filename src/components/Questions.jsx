2d832c434aba305a59e8404b7ea842b3fe2345d1
StphnX <136083929+StphnX@users.noreply.github.com>
2023-07-26 13:37:46 +0200

    added changes to functionality (JS)

---
+++ src/components/Questions.jsx
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    text: "What is the highest peak in Germany?",
+    options: [
+      { id: 0, text: "Medium Weather Peak", isCorrect: false },
+      { id: 1, text: "Schneefernerkopf", isCorrect: false },
+      { id: 2, text: "Hochwanner", isCorrect: false },
+      { id: 3, text: "Zugspitze", isCorrect: true },
+    ],
+  },
+  {
+    text: "Palm Island is in which Country?",
+    options: [
+      { id: 0, text: "United Arab Emirates - Dubai", isCorrect: true },
+      { id: 1, text: "United states of America - Florida", isCorrect: false },
+      { id: 2, text: "South America", isCorrect: false },
+      { id: 3, text: "North America", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the largest state in the germany?",
+    options: [
+      { id: 0, text: "Bavaria", isCorrect: true },
+      { id: 1, text: "Niedersachsen", isCorrect: false },
+      { id: 2, text: "Baden-Württemberg", isCorrect: false },
+      { id: 3, text: "Hessen", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the largest state in the US?",
+    options: [
+      { id: 0, text: "California", isCorrect: false },
+      { id: 1, text: "Alaska", isCorrect: true },
+      { id: 2, text: "Texas", isCorrect: false },
+      { id: 3, text: "Montana", isCorrect: false },
+    ],
+  },
+  {
+    text: "Which of the following countries DO NOT border the US?",
+    options: [
+      { id: 0, text: "Canada", isCorrect: false },
+      { id: 1, text: "Russia", isCorrect: true },
+      { id: 2, text: "Cuba", isCorrect: true },
+      { id: 3, text: "Mexico", isCorrect: false },
+    ],
+  },
+];
+
+export default questions;
