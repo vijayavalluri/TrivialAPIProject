195d23629e3d8dd01e08641bc275bad2114afce3
vijayavalluri <vvvijaya56@gmail.com>
2023-07-26 08:50:00 +0200

    'Basic Questions'

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Questions from './components/Questions'
 
 function App() {
 
   return (
     <>
-
+        <Questions/>
     </>
   )
 }
---
+++ src/components/Questions.jsx
@@ -0,0 +1,42 @@
+
+function Questions() {
+
+
+return (
+    <div>
+    <h2>
+    <h3 className="question-text">What is the highest peak in Germany?</h3>
+    <ul>
+      <li>Schneefernerkopf</li>
+      <li>Medium Weather Peak</li>
+      <li>Zugspitze</li>
+      <li>Hochwanner</li>
+    </ul>
+    </h2>
+
+
+    <h2>
+    <h3 className="question-text">Palm Island is in which Country?</h3>
+    <ul>
+      <li>United states of America - Florida</li>
+      <li>South America</li>
+      <li>North America</li>
+      <li>United Arab Emirates - Dubai</li>
+    </ul>
+    </h2>
+
+    <h2>
+    <h3 className="question-text">What is the largest state in the germany?</h3>
+    <ul>
+      <li>Bavaria</li>
+      <li> Niedersachsen</li>
+      <li> Baden-Württemberg</li>
+      <li> Hessen</li>
+    </ul>
+    </h2>
+
+    
+  </div>
+);
+}
+export default Questions
