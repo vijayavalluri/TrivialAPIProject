dd10422060b74c6e0f5104117347bff558e71b58
vijayavalluri <vvvijaya56@gmail.com>
2023-07-27 14:09:10 +0200

    'file name corrected'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from 'react-dom';
-import ClickWithSound from './ClickWithSound';
+import ClickableWithSound from './ClickableWithSound';
 import "./App.css";
 import axios from 'axios'
 // import questions from './components/Questions'
@@ -225,7 +225,7 @@ function App() {
             ) : (
               /* 5. Question Card  */
               <div className="question-card">
-                      <ClickWithSound />
+                      <ClickableWithSound />
                 {/* Current Question  */}
                 <h2>
                   Question: {currentQuestion + 1} out of {questions.length}
